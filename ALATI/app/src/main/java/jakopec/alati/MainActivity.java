7b344d6176bdfea61b49d93a093ba471b255b47e
Tomislav Jakopec <tjakopec@gmail.com>
2020-02-24 15:12:36 +0100

    Zavrpetak Android alati

--- ALATI/app/src/main/java/jakopec/alati/MainActivity.java
+++ ALATI/app/src/main/java/jakopec/alati/MainActivity.java
@@ -8,10 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
-
-import java.util.List;
 
 import butterknife.BindString;
 import butterknife.BindView;
